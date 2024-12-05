228bfd1d6d9bdd750733859a75ab8777e7eba705
Whitlcw <58365468+Whitlcw@users.noreply.github.com>
2024-12-04 19:13:52 -0700

    added linearized dynamics simulation

--- Part1_Sec3.m
+++ Part1_Sec3.m
@@ -51,7 +51,6 @@ dynamics = @(t, x) x_dotODE45(t, x, u_func,L);
 %PERTURBATION ADDED
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [t, x] = ode45(dynamics, t_span, x_init + perturb_x0); %perturbation added
-%[t, x] = ode45(dynamics, t_span, x_init); %use if perturbation unwanted
 
 % Wrap the angles theta_g (x(3,:)) and theta_a (x(6,:)) to [-pi, pi]
 x(:, 3) = mod(x(:, 3) + pi, 2*pi) - pi;  % Wrap theta_g (ground heading)
@@ -89,10 +88,104 @@ end
 sgtitle('State Trajectories over Time With Perturbation', 'Interpreter', 'latex');
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Simulate using LTV DT Model
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% solve nominal trajectory with no perturbations
+[t, x] = ode45(dynamics, t_span, x_init);
 
+% initial conditions for perturbation state vector
+x_sim = perturb_x0';
 
+% simulate system behavior at each time step k
+for k = 1:1:1000
+    % Use CT jacobians to find A and B at t = t_k and nom[k]
+    A_nom_k = get_Abar(u_init, x(k, :)');
+    B_nom_k = get_Bbar(u_init, x(k, :)', L);
 
+    % Eulerized estimate of DT Jacobians
+    F_k = dt * A_nom_k;
+    G_k = dt * B_nom_k;
+
+    % Calculate perturbation state at t = t_k
+    x_k = F_k * x_sim(k, :)' + G_k * u_init;
+    x_sim = [x_sim; x_k'];
+end
+
+% find total state by combining nominal trajectory with perturbation states
+x_total = x + x_sim;
+
+% Wrap the angles theta_g (x_total(3,:)) and theta_a (x_total(6,:)) to [-pi, pi]
+x_total(:, 3) = mod(x_total(:, 3) + pi, 2*pi) - pi;  % Wrap theta_g (ground heading)
+x_total(:, 6) = mod(x_total(:, 6) + pi, 2*pi) - pi;  % Wrap theta_a (air heading)
+
+% Plot total state results
+figure;
+for i = 1:size(x_total, 2)
+    subplot(size(x_total, 2), 1, i);
+    plot(t, x_total(:, i), 'LineWidth', 1.5);
+    grid on;
+    
+    % Adjust titles and labels based on the state variables
+    if i == 1
+        title('$\xi$ (Easting of ground)', 'Interpreter', 'latex');
+        ylabel('$\xi$ (m)', 'Interpreter', 'latex');
+    elseif i == 2
+        title('$\eta$ (Northing of ground)', 'Interpreter', 'latex');
+        ylabel('$\eta$ (m)', 'Interpreter', 'latex');
+    elseif i == 3
+        title('$\theta$ (Heading of ground)', 'Interpreter', 'latex');
+        ylabel('$\theta$ (rad)', 'Interpreter', 'latex');
+    elseif i == 4
+        title('$\xi$ (Easting of air)', 'Interpreter', 'latex');
+        ylabel('$\xi$ (m)', 'Interpreter', 'latex');
+    elseif i == 5
+        title('$\eta$ (Northing of air)', 'Interpreter', 'latex');
+        ylabel('$\eta$ (m)', 'Interpreter', 'latex');
+    elseif i == 6
+        title('$\theta$ (Heading of air)', 'Interpreter', 'latex');
+        ylabel('$\theta$ (rad)', 'Interpreter', 'latex');
+    end
+    xlabel('Time (s)', 'Interpreter', 'latex');
+end
+sgtitle('States vs. Time, Linearized Approximate Dynamics Simulation', 'Interpreter', 'latex');
+
+% plot perturbation state results
+% Wrap the angles theta_g (x_total(3,:)) and theta_a (x_total(6,:)) to [-pi, pi]
+x_sim(:, 3) = mod(x_sim(:, 3) + pi, 2*pi) - pi;  % Wrap theta_g (ground heading)
+x_sim(:, 6) = mod(x_sim(:, 6) + pi, 2*pi) - pi;  % Wrap theta_a (air heading)
+
+% Plot results
+figure;
+for i = 1:size(x_total, 2)
+    subplot(size(x_sim, 2), 1, i);
+    plot(t, x_sim(:, i), 'LineWidth', 1.5);
+    grid on;
+    
+    % Adjust titles and labels based on the state variables
+    if i == 1
+        title('$\xi$ (Easting of ground)', 'Interpreter', 'latex');
+        ylabel('$\xi$ (m)', 'Interpreter', 'latex');
+    elseif i == 2
+        title('$\eta$ (Northing of ground)', 'Interpreter', 'latex');
+        ylabel('$\eta$ (m)', 'Interpreter', 'latex');
+    elseif i == 3
+        title('$\theta$ (Heading of ground)', 'Interpreter', 'latex');
+        ylabel('$\theta$ (rad)', 'Interpreter', 'latex');
+    elseif i == 4
+        title('$\xi$ (Easting of air)', 'Interpreter', 'latex');
+        ylabel('$\xi$ (m)', 'Interpreter', 'latex');
+    elseif i == 5
+        title('$\eta$ (Northing of air)', 'Interpreter', 'latex');
+        ylabel('$\eta$ (m)', 'Interpreter', 'latex');
+    elseif i == 6
+        title('$\theta$ (Heading of air)', 'Interpreter', 'latex');
+        ylabel('$\theta$ (rad)', 'Interpreter', 'latex');
+    end
+    xlabel('Time (s)', 'Interpreter', 'latex');
+end
+sgtitle('Linearized Approx Perturbations vs. Time', 'Interpreter', 'latex');
 
 
 
